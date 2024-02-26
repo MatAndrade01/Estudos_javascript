a591c2353c08cbf75171cb2e529bd3f6970b6e16
Matheus Andrade <ltbmatheus141012@gmail.com>
2024-02-26 16:21:51 -0300

    Aprendendo sobre fuções

---
+++ aula16/funçaõ01.js
@@ -0,0 +1,14 @@
+function parimpa(n) {
+    if(n % 2 == 0){
+        return 'par!'
+    }
+
+    else {
+        return 'Ímpar!'
+    }
+}
+
+/*let res = parimpa(4)
+console.log(res)*/
+
+console.log(parimpa(223))
